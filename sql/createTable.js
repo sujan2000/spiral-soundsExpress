6f309c5ed20f8b5e1f8ad02b9a48fb19ca363563
sujan2000 <meena.sujan06@gmail.com>
2026-01-21 21:34:20 +0530

    cart_items db table add

--- sql/createTable.js
+++ sql/createTable.js
@@ -35,9 +35,20 @@ async function createTable() {
        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
        UNIQUE(email, username)
         )
-        `)
+        `);
 
-    console.log('Table products and users created with UNIQUE constraint');
+    await db.exec(`
+            CREATE TABLE IF NOT EXISTS cart_items (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER NOT NULL,
+              product_id INTEGER NOT NULL,
+              quantity INTEGER NOT NULL DEFAULT 1,
+              FOREIGN KEY (user_id) REFERENCES users(id),
+              FOREIGN KEY (product_id) REFERENCES products(id)
+            )
+            `);
+
+    console.log('Table products, users and cart_items created with UNIQUE constraint');
     await db.close();
 }
 createTable()
